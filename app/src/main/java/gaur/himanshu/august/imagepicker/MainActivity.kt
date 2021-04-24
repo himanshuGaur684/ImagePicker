72ffc45d2a1cb98f325da6b774b6e8011dfe4248
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2021-04-24 14:08:03 +0530

    Initial commit

--- app/src/main/java/gaur/himanshu/august/imagepicker/MainActivity.kt
+++ app/src/main/java/gaur/himanshu/august/imagepicker/MainActivity.kt
@@ -10,41 +10,43 @@ import com.github.dhaval2404.imagepicker.ImagePicker
 
 class MainActivity : AppCompatActivity() {
 
-    var imageView: ImageView? = null
+    var imagePicker: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        imagePicker = findViewById(R.id.picker_image)
 
-        imageView = findViewById(R.id.picker_image)
         val gallery = findViewById<Button>(R.id.gallery)
         val camera = findViewById<Button>(R.id.camera)
 
-
-        camera.setOnClickListener {
-
-            ImagePicker.with(this).cameraOnly().crop().maxResultSize(400,400).start()
-
-        }
         gallery.setOnClickListener {
 
             ImagePicker.with(this).galleryOnly().galleryMimeTypes(arrayOf("image/*")).crop()
                 .maxResultSize(400, 400).start()
+
         }
 
+        camera.setOnClickListener {
+
+            ImagePicker.with(this).cameraOnly().crop().maxResultSize(400, 400).start()
+
+        }
 
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(resultCode== Activity.RESULT_OK && requestCode== ImagePicker.REQUEST_CODE){
+        if(resultCode== Activity.RESULT_OK && requestCode== ImagePicker.REQUEST_CODE) {
 
-            imageView?.setImageURI(data?.data)
+
+            imagePicker?.setImageURI(data?.data)
 
         }
 
     }
+
 
 }
